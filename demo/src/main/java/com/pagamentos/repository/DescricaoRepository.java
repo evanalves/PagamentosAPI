598aa789b67f7579c99387510691ab3e391ad649
evanalves <49462793+evanalves@users.noreply.github.com>
2022-04-11 19:36:28 -0300

    PagamentoApp

--- demo/src/main/java/com/pagamentos/repository/DescricaoRepository.java
+++ demo/src/main/java/com/pagamentos/repository/DescricaoRepository.java
@@ -1,9 +1,10 @@
 package com.pagamentos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pagamentos.domain.Descricao;
-
+@Repository
 public interface DescricaoRepository extends JpaRepository<Descricao, Long> {
 
 }
--- demo/src/main/java/com/pagamentos/repository/FormaPagamentoRepository.java
+++ demo/src/main/java/com/pagamentos/repository/FormaPagamentoRepository.java
@@ -1,9 +1,10 @@
 package com.pagamentos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.pagamentos.domain.FormaPagamento;
-
+@Repository
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 
 }
